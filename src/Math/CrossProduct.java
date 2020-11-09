3fe804569e1e155b5f441b1ded1c6c511f4d2969
Sofia Bzhilyanskaya <sab4633@rit.edu>
2020-11-09 17:09:16 -0500

    Cross product method created and now just needs to be tested

--- src/Math/CrossProduct.java
+++ src/Math/CrossProduct.java
@@ -8,10 +8,21 @@ import java.util.List;
 public class CrossProduct {
 
     //fields
-    private List<Double> l1;
-    private List<Double> l2;
+    private List<Double> vect1;
+    private List<Double> vect2;
 
     //constructor
+    public CrossProduct(List<Double> l1,  List<Double> l2){
+        vect1 = l1;
+        vect2 = l1;
+    }
 
     //methods
+    public List<Double> solver(){
+        List<Double> answer = null;
+        answer.add((vect1.get(1)*vect2.get(2))-(vect2.get(1)*vect1.get(2)));
+        answer.add(-1*(vect1.get(0)*vect2.get(2))-(vect2.get(0)*vect1.get(2)));
+        answer.add((vect1.get(0)*vect2.get(1))-(vect2.get(0)*vect1.get(1)));
+        return answer;
+    }
 }
